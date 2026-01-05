1a86baf77693f772da09a2db8064cda304b319f2
hindveer.singh <hindveer.singh@ppreciate.com>
2026-01-05 18:07:30 +0530

    security filter

--- src/main/java/com/movie/demo/Security/SecurityConfig.java
+++ src/main/java/com/movie/demo/Security/SecurityConfig.java
@@ -1,14 +1,24 @@
 package com.movie.demo.Security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 public class SecurityConfig
 {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
     private final JwtAuthFilter jwtAuthFilter;
+
+
 
     public SecurityConfig(JwtAuthFilter jwtAuthFilter)
     {
